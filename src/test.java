dee8c0ea43a2dec3925cf510e9f29363cfb70c8e
seb <sebastienneumann78@gmail.com>
2015-02-10 16:10:50 +0100

    premier commit

---
+++ src/test.java
@@ -0,0 +1,14 @@
+/**
+ * Created by seb on 10/02/2015.
+ */
+public class test {
+
+
+    public static void main (String argv[]){
+        System.out.println("### Starting TEST ###");
+
+
+    }
+
+
+}
